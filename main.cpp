3f3073a49c6d4414b4ff1993bdb0bfe04c9996ef
claudiaonofrei <clau.onofrei@yahoo.com>
2017-01-13 23:48:29 +0200

    nu merge exact cum ar trebui

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include<stdlib.h>
 #include<string.h>
@@ -5,11 +6,12 @@
 
 using namespace std;
 int vec[15],manaJucatorului[10],manaDealerului[10],modul=0,punctajManaJucator=0,punctajManaDealer=0,numarCartiJucator=0,numarCartiDealer=0,alege=0,punctajJucator1=0,punctajJucator2=0;
-int pachetCarti[14],manaJucator1[10],manaJucator2[10];
+int pachetCarti[14],manaJucator1[10],manaJucator2[10],nrCartiJucator1=0,nrCartiJucator2=0,tasta=0;
 char alegere,nume[15],nume1[15];
+
 int modulJocului()
 {
-    int modul=0;
+    int modul=0,punctajManaDealer=punctajManaJucator=0;
     if(modul!=1||modul!=2)
     {
         cout<<"Alegeti modul in care doriti sa jucati! \n";
@@ -39,6 +41,7 @@ int modulJocului()
 
     return modul;
 }
+
 void cineCastiga(int punctajManaJucator, int punctajManaDealer)
 {
     if(punctajManaJucator>21)
@@ -81,17 +84,6 @@ int valoareaCartilor(int carte)
    }
     return valoare;
 }
-int scorMana(int mana[],int nrcarti,int card)
-{   int scor=0;
-    for(int i=0;i<nrcarti;i++)
-    {
-        if(mana[i]=='J'||mana[i]=='Q'||mana[i]=='K')
-            scor=scor+10;
-        else if(mana[i]=='A')
-            scor=scor+11;
-    }
-    return scor;
-}
 
 void afiseazaCartea(int carti)
 {
@@ -109,6 +101,7 @@ void afiseazaCartea(int carti)
         {cout<<carti;}
     }
 }
+
 void arataMana(int mana[],int nrcarti)
 {
     int i=0,cealaltaCarte=0;
@@ -120,6 +113,7 @@ void arataMana(int mana[],int nrcarti)
     }
     cout<<endl;
 }
+
 int carteaUrmatoare(int pachetCarti[])
 {
     int cartenoua=0,ok=1;
@@ -134,6 +128,7 @@ int carteaUrmatoare(int pachetCarti[])
     }
     return cartenoua;
 }
+
 void afisareMeniu()
 {
     system("cls");
@@ -143,8 +138,6 @@ void afisareMeniu()
     cout<< "|*|*|*|*|*|*|*|*|*|*|*|*|*|" <<endl;
     cout<<endl;
     cout<< "Bine ati venit la BlackJack" <<endl;
-    cout<< "Apasati tasta enter pentru a incepe jocul"<<endl;
-    cin.get();
 }
 
 int main()
@@ -155,7 +148,7 @@ int main()
     srand(tm);
     afisareMeniu();
     alege=modulJocului();
-    if(alege==1)
+    while(alege==1)
     {
         numarCartiJucator=2;
         numarCartiDealer=2;
@@ -167,26 +160,58 @@ int main()
             punctajManaDealer+=valoareaCartilor(manaDealerului[i]);
         }
 
-        cout<<"Cartile Dealerului sunt:"<<"[]"<<" ";
+        cout<<"Cartile Dealerului sunt:"<<"*"<<" ";
         cout<<manaDealerului[1]<<endl;
         cout<<endl;
-        cout<<"Cartile jucatorului sunt:";
+        cout<<nume<<", cartile tale sunt: ";`
        arataMana(manaJucatorului,numarCartiJucator);
+        cout<<endl;
         cout<<"Punctajul tau este:"<<" ";
         cout<<punctajManaJucator<<endl;
         cout<<endl;
+        if(punctajManaJucator==21)
+            cout<<"Felicitari!!"<<nume<<" ai castigat!";
+        if(punctajManaDealer==21)
+            cout<<"Computerul a castigat!!";
+
+
         cout<<"Daca mai doriti inca o carte, apasati tasta 'H'. In caz contrar, apasati tasta 'S'"<<endl;
         cin>>alegere;
-        if(alegere=='H'||alegere=='h')
+        while(alegere=='H'||alegere=='h')
         {
             manaJucatorului[numarCartiJucator]=carteaUrmatoare(pachetCarti);
+            punctajManaJucator+=valoareaCartilor(manaJucatorului[numarCartiJucator]);
             numarCartiJucator++;
+            arataMana(manaJucatorului,numarCartiJucator);
+            cout<<"Punctajul tau este:";
+            cout<<punctajManaJucator<<endl;
+
+            if(punctajManaDealer<17);
+                {
+                    manaDealerului[numarCartiDealer]=carteaUrmatoare(pachetCarti);
+                    punctajManaDealer+=valoareaCartilor(manaDealerului[numarCartiDealer]);
+                    numarCartiDealer++;
+                }
+
+            cin>>alegere;
         }
-        else if(alegere=='S'||alegere=='s')
-            cineCastiga(punctajManaJucator,punctajManaDealer);
-        else cout<<"Ati ales o valoare eronata!!Mai incercati o data!"<<endl;
+
+        if(alegere=='S'||alegere=='s')
+               {
+                    arataMana(manaDealerului,numarCartiDealer);
+                    cout<<endl;
+                    cout<<"punctajul Dealerului este:"<<punctajManaDealer<<endl;
+                    cout<<"Punctajul jucatorului:"<<punctajManaJucator<<endl;
+                    cineCastiga(punctajManaJucator,punctajManaDealer);
+                    cout<<endl;
+               }
+            else cout<<"Ati ales o valoare eronata!!Mai incercati o data!"<<endl;
+            alege=modulJocului();
+            cin>>alege;
+
+
     }
-    if(alege==2)
+     if(alege==2)
     {
         for(int i=0;i<2;i++)
         {
@@ -195,14 +220,72 @@ int main()
             manaJucator2[i]=carteaUrmatoare(pachetCarti);
             punctajJucator2=punctajJucator2+valoareaCartilor(manaJucator2[i]);
         }
-            cout<<"Carti " << nume << ":";
+        nrCartiJucator1=2;
+        nrCartiJucator2=2;
+            cout<<nume<< ","<<" cartile tale sunt : ";
             arataMana(manaJucator1,2);
-            cout<<"Carti " << nume1 << ":";
+            cout<<nume1<< ","<<" cartile tale sunt: ";
             arataMana(manaJucator2,2);
             cout<<"Punctaj " << nume<<":";
             cout<<punctajJucator1<<endl;
+            cout<<endl;
             cout<<"Punctaj " << nume1<<":";
             cout<<punctajJucator2<<endl;
+            cout<<endl;
+            cout<<nume<<","<<" apasa:"<<endl;
+            cout<<" 1 pentru a mai trage o carte"<<endl;
+            cout<<" 2 pentru a te opri"<<endl;
+            cout<<" 3 pentru EXIT"<<endl;
+            cin>>tasta;
+            if(tasta==1)
+            {
+                manaJucator1[nrCartiJucator1]=carteaUrmatoare(pachetCarti);
+                punctajJucator1+=valoareaCartilor(manaJucator1[nrCartiJucator1]);
+                nrCartiJucator1++;
+                arataMana(manaJucator1,nrCartiJucator1);
+                cout<<"Punctajul tau este:";
+                cout<<punctajJucator1;
+            }
+            else if(tasta==2)
+            {
+                if(punctajJucator1>21)
+                {
+                    cout<<nume<<",";
+                    cout<<"ai pierdut. Mai incearca o data"<<endl;
+                }
+                else
+                    if(punctajJucator1==punctajJucator2)
+                {
+                    cout<<"Este egalitate!!";
+                }
+                else
+                    if(punctajJucator1>punctajJucator2&&punctajJucator1<21)
+                {
+                    cout<<"Felicitari, "<<nume;
+                    cout<<" .Erai atat de aproape de 21, dar totusi ai reusit!";
+                }
+                else
+                    if(punctajManaJucator==21)
+                {
+                    cout<<"|*|*|*|*|*|*|*|*|*|*|*|*|"<<endl;
+                    cout<<"Felicitari!! "<<nume;
+                    cout<<" Ai castigat!"<<endl;
+                }
+                else cout<<nume1<<" a castigat!!!";
+
+            }
+            else if(tasta==3)
+                {
+                    exit (0);
+                    cout<<nume<<" a parasit jocul!"<<endl;
+                    cout<<nume1<<" a castigat!"<<endl;
+                }
+
+
+            else cout<<"Ai ales o valoare gresita. Apasa din nou!";
+
 
     }
+    return 0;
 }
+
